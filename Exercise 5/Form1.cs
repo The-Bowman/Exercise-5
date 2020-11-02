de8adcb0be5045c3fcf6f275dc9b63ca94e4576b
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2020-11-01 20:43:02 -0700

    added comments

--- Exercise 5/Form1.cs
+++ Exercise 5/Form1.cs
@@ -20,15 +20,23 @@ namespace Exercise_5
 
         private void calculateButton_Click(object sender, EventArgs e)
         {
-            int n;
-            double pi = 4;
+            int n; 
+            double pi = 4;  //starting value
 
+            //attempts to parse string input to int
             if (int.TryParse(numberOfTermsTextbox.Text, out n))
             {
+                //displays number of operations to be performed
                 numberOfTermsApproxLabel.Text = "Approximate value of Pi after " + n + " terms.";
+
+                //loops the number of times specified by the user
+                //each iteration adjusts denominator
                 for (int i = 0; i < n; i++)
                 {
                     double denom = i * 2 + 3;
+
+                    //determines whether the iteration is even or odd
+                    //even means subtract, odd means add
                     if (i % 2 == 0)
                     {
                         pi -= (4 / denom);
